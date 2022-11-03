142a9ed9120ed97eff067acd4edfe70fee676565
Florian Rädiker <github@florian-raediker.de>
2022-11-03 22:57:41 +0100

    [Midnights] Change bg colors to neutral TODO: Change to lavender

--- src/components/modals/BaseModal.tsx
+++ src/components/modals/BaseModal.tsx
@@ -27,7 +27,7 @@ export const BaseModal = ({ title, children, isOpen, handleClose }: Props) => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Dialog.Overlay className="fixed inset-0 min-h-screen bg-gray-500 bg-opacity-75 transition-opacity" />
+            <Dialog.Overlay className="fixed inset-0 min-h-screen bg-gray-500 bg-opacity-75 transition-opacity dark:bg-neutral-700 dark:bg-opacity-75" />
           </Transition.Child>
           <Transition.Child
             as={Fragment}
@@ -38,7 +38,7 @@ export const BaseModal = ({ title, children, isOpen, handleClose }: Props) => {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all dark:bg-gray-800 sm:my-8 sm:w-full sm:max-w-sm sm:p-6 sm:align-middle">
+            <div className="inline-block transform overflow-hidden rounded-lg bg-white px-4 pt-5 pb-4 text-left align-bottom shadow-xl transition-all dark:bg-neutral-900 sm:my-8 sm:w-full sm:max-w-sm sm:p-6 sm:align-middle">
               <button
                 onClick={() => handleClose()}
                 tabIndex={0}
