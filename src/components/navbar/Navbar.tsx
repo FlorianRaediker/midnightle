a362f30a0cc3aad2b80cc37a6cf05547c7722d62
Florian Rädiker <github@florian-raediker.de>
2022-11-04 03:01:23 +0100

    Improve navbar height

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -22,7 +22,7 @@ export const Navbar = ({
   setIsSettingsModalOpen,
 }: Props) => {
   return (
-    <div className="mx-5 flex h-12 items-center justify-between sm:mx-auto sm:w-[550px] short:h-auto">
+    <div className="mx-5 flex h-12 items-center justify-between sm:mx-auto sm:w-[550px] short:h-10">
       <div className="flex w-16 pr-2">
         <InformationCircleIcon
           className="h-6 w-6 cursor-pointer dark:stroke-white"
